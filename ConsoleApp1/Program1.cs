3f2fe79598f4c6f669479d6cefb88eba9165ed53
jufren <tan.jufren@stengg.com>
2018-08-29 13:52:03 +0800

    Add project files.

---
+++ ConsoleApp1/Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    static class Program1
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            string body = File.ReadAllText("data.json");
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "94650262681e448cb771cc79c4a1f375");
+
+            var uri = "https://southeastasia.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(body);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+            }
+
+        }
+    }
+}
